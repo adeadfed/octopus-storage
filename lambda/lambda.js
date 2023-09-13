17622a32e1f54083270b2ba2d0e4ecf4569d4fd1
adeadfed <maksym.vatsyk17@gmail.com>
2023-09-13 22:13:13 +0200

    update lambda code

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,31 +1,51 @@
 const AWS = require('aws-sdk')
 const COGNITO_GROUP = 'aws-cognito-bad-practice-list-users-group'
-const COGNITO_POOL_ID = 'us-east-2_mDdinQbcT' 
-
 
 exports.handler = (event, context, callback) => {
     console.log(event);
     
     let cognitoIdp = new AWS.CognitoIdentityServiceProvider();
-    
-    let username = event.userName;
-    
-    let params = {
-        GroupName: COGNITO_GROUP,
-        UserPoolId: COGNITO_POOL_ID,
-        Username: username
-    }
-    
-    cognitoIdp.adminAddUserToGroup(params, function(err, _) {
+
+    // dynamically resolve user pool id just because
+    // we don't want to run terraform apply 2 times
+    cognitoIdp.listUserPools({
+        MaxResults: 20
+    },
+    function(err, data) {
         if (err) {
             console.log(err, err.stack);
         }
-        else {
-            // implement custom logging for our beautiful app here!!!
-            logger = Function('date', `console.log("Added user ${username} to the group at: " + date`);
-            logger(new Date());
-            
-            callback(null, event);
+        
+        const COGNITO_POOL_ID = data.UserPools[0].Id 
+
+        // auto verify users on signup
+        event.response.autoConfirmUser = true
+
+        if ('email' in event.request.userAttributes){
+            event.response.autoVerifyEmail = true
         }
+
+        if ('phone_number' in event.request.userAttributes){
+            event.response.autoVerifyPhone = true
+        }
+
+        // add user to the list users group
+        cognitoIdp.adminAddUserToGroup({
+            GroupName: COGNITO_GROUP,
+            UserPoolId: COGNITO_POOL_ID,
+            Username: event.userName
+        }, 
+        function(err, _) {
+            if (err) {
+                console.log(err, err.stack);
+            }
+            else {
+                // implement custom logging for our beautiful app here!!!
+                logger = Function('date', `console.log("Added user ${event.userName} to the group at: " + date)`);
+                logger(new Date());
+                
+                context.succeed(event);
+            }
+        })
     })
 }
