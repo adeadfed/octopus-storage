27424a60331a4f66f86e7e80ce371efa65a8bf10
adeadfed <maksym.vatsyk17@gmail.com>
2023-10-17 00:56:17 +0200

    rename templates, add attr app template

--- tf/templates.tf
+++ tf/templates.tf
@@ -1,6 +1,6 @@
 # frontend app JS config
-data "template_file" "frontend_js_config" {
-  template = file("./config_templates/config.js.tmpl")
+data "template_file" "frontend_js_config_template" {
+  template = file("./templates/config.js.tmpl")
   vars = {
     region_id        = "us-east-1"
     identity_pool_id = aws_cognito_identity_pool.octopus_identity_pool.id
@@ -16,9 +16,8 @@ data "template_file" "frontend_js_config" {
 }
 
 # admin app userdata config
-
-data "template_file" "admin_userdata_config" {
-  template = file("./config_templates/userdata.sh.tmpl")
+data "template_file" "admin_userdata_config_template" {
+  template = file("./templates/userdata.sh.tmpl")
   vars = {
     region_id = "us-east-1"
 
@@ -29,4 +28,20 @@ data "template_file" "admin_userdata_config" {
     aws_access_key_id     = aws_iam_access_key.admin_octopus_storage_service_key.id
     aws_secret_access_key = aws_iam_access_key.admin_octopus_storage_service_key.secret
   }
+}
+
+# attributes app file
+data "template_file" "attributes_app_template" {
+  template = file("./templates/app.py.tmpl")
+  vars = {
+    region_id           = "us-east-1"
+    user_pool_id        = aws_cognito_user_pool.octopus_user_pool.id
+    user_pool_client_id = aws_cognito_user_pool_client.attributes_app_client.id
+  }
+}
+
+# write attributes app file on the FS
+resource "local_file" "attributes_app_file" {
+  content  = data.template_file.attributes_app_template.rendered
+  filename = "../user-pool-attributes-app/app.py"
 }
