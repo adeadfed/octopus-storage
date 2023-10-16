441e1e2294274698cce3621184ce8187f692a523
adeadfed <maksym.vatsyk17@gmail.com>
2023-10-16 23:43:14 +0200

    use static ubuntu AMI to avoid breaking things

--- tf/ec2.tf
+++ tf/ec2.tf
@@ -8,18 +8,18 @@ resource "aws_key_pair" "octopus_admin_ssh_key" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  filter {
-    name   = "name"
-    values = ["ubuntu*"]
-  }
-}
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["amazon"]
+#   filter {
+#     name   = "architecture"
+#     values = ["arm64"]
+#   }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu*"]
+#   }
+# }
 
 resource "aws_security_group" "octopus_admin_web_app_sg" {
   name        = "octopus-admin-web-app-sg-${random_id.tf_id.hex}"
@@ -48,7 +48,8 @@ resource "aws_security_group" "octopus_admin_web_app_sg" {
 }
 
 resource "aws_instance" "octopus_admin_web_app_ec2" {
-  ami           = data.aws_ami.ubuntu.id
+  # ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-0a0c8eebcdd6dcbd0" # Canonical, Ubuntu, 22.04 LTS, arm64 jammy image build on 2023-05-16
   instance_type = "t4g.nano"
   user_data     = data.template_file.admin_userdata_config.rendered
   key_name      = aws_key_pair.octopus_admin_ssh_key.key_name
